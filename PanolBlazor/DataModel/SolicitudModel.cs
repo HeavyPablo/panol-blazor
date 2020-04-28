e6614aa2e6a8cc9a47ee9b3f2a45487f30e32136
Mario Bustos <pabloasd3@gmail.com>
2020-04-28 02:39:33 -0400

    Editar Solicitud

--- PanolBlazor/DataModel/SolicitudModel.cs
+++ PanolBlazor/DataModel/SolicitudModel.cs
@@ -18,5 +18,39 @@ namespace PanolBlazor.DataModel
         public PanoleroModel panolero { get; set; }
         public List<ProductoModel> productos { get; set; }
 
+        public string EditFormatJson(SolicitudModel Solicitud, string UsernameUsuario, string RutResponsable, List<ProductoModel> Productos)
+        {
+            string productos = "";
+            int length = Productos.Count();
+            ProductoModel ultimoProducto = Productos[length - 1];
+
+            foreach (var producto in Productos)
+            {
+                productos += "{" +
+                                @"""id"":""" + producto.id + @"""" +
+                            "}";
+                if (producto != ultimoProducto)
+                {
+                    productos += ",";
+                }
+            }
+
+            string json =   "{" +
+                                @"""solicitud"": [" +
+                                        "{" +
+                                            @"""comentario"":""" + Solicitud.comentario + @"""," +
+                                            @"""estado"":""" + Solicitud.estado + @"""," +
+                                            @"""tipo"":""" + Solicitud.tipoSolicitud + @"""," +
+                                            @"""solicituante"":""" + UsernameUsuario + @"""," +
+                                            @"""responsable"":""" + RutResponsable + @"""" +
+                                        "}" +
+                                    "]," +
+                                @"""productos"": [" +
+                                            productos +
+                                    "]" +
+                            "}";
+
+            return json;
+        }
     }
 }
