de7ed768cb182fc15e750578c68faadf7d9efae8
Mario Bustos <pabloasd3@gmail.com>
2020-05-06 01:29:34 -0400

    Agregar Solicitud - Avances

---
+++ PanolBlazor/FormModel/FiltrosChangeUsuarioModalModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PanolBlazor.FormModel
+{
+    public class FiltrosChangeUsuarioModalModel
+    {
+        
+        public string FiltroBusqueda { get; set; }
+        public string FiltroPerfil { get; set; }
+        public string FiltroEstado { get; set; }
+    }
+}
