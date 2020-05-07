23a03240e7d701442a2bae76bc2b0db99cff1819
Mario Bustos <pabloasd3@gmail.com>
2020-05-07 19:50:20 -0400

    Solicitud - filtros para productos

---
+++ PanolBlazor/FormModel/FiltrosProductoModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PanolBlazor.FormModel
+{
+    public class FiltrosProductoModel
+    {
+        public string Subcategoria { get; set; }
+        public string Categoria { get; set; }
+        public string Nombre { get; set; }
+    }
+}
