bdd008081106864707d0443697eafbd0dfce479d
Mario Bustos <pabloasd3@gmail.com>
2020-05-19 04:36:29 -0400

    Mejoras visuales - Home, solicitudes y productos

---
+++ PanolBlazor/FormModel/FiltrosSolicitudesModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PanolBlazor.FormModel
+{
+    public class FiltrosSolicitudesModel
+    {
+        public string perfilSolicitante { get; set; }
+        public string nombreSolicitante { get; set; }
+    }
+}
